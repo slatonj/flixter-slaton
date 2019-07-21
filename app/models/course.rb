87fafb009b36b89216c8b10e1d936ac535ef828e
jeff <jeffslaton@msncom>
2019-07-21 11:34:01 +0000

    added enrollment and edit course for enrollment

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,8 @@
 class Course < ApplicationRecord
     belongs_to :user
     has_many :sections
+    has_many :enrollments
+
 
     validates :title, presence: true
     validates :description, presence: true
