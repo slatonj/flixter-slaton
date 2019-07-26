13c42f75dd10ba37501791a069fd9f7cac56d69a
jeff <jeffslaton@msncom>
2019-07-26 14:57:02 +0000

    fixed added section page and did some work on the master.scss to improve the over all look of course page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,7 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, :only [:create]
-  before_action :require_authorized_for_current_section. :only [:update]
+  before_action :require_authorized_for_current_course, :only => [:create]
+  before_action :require_authorized_for_current_section, :only => [:update]
 
   def new
       @section = Section.new
@@ -14,7 +14,7 @@ class Instructor::SectionsController < ApplicationController
 
     def update
       current_section.update_attributes(section_params)
-      render :text 'Updated!'
+      render plain: 'Updated!'
       
     end
     
@@ -24,8 +24,9 @@ class Instructor::SectionsController < ApplicationController
 
     def require_authorized_for_current_section
 			if current_section.course.user != current_user
-				return render plain: 'Unauthorized', status: :unauthorized
+			   render plain: 'Unauthorized', status: :unauthorized
       end
+    end
 
       
       def current_section
@@ -43,9 +44,14 @@ class Instructor::SectionsController < ApplicationController
 
 
     helper_method :current_course
+
     def current_course
+      if params[:course_id]
       @current_course ||= Course.find(params[:course_id])
+      else
+        current_section.course
     end
+  end
 
     def section_params
       params.require(:section).permit(:title, :row_order_position)
